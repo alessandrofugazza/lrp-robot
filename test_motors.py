c0d5e1cb9e13f8e945d0cc3e9c6d594726585d96
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2024-12-24 00:35:03 +0100

    use constants

--- test_motors.py
+++ test_motors.py
@@ -2,9 +2,16 @@ from Raspi_MotorHAT import Raspi_MotorHAT
 import time
 import atexit
 
-mh = Raspi_MotorHAT(addr=0x60)
-lm = mh.getMotor(3)
-rm = mh.getMotor(4)
+I2C_ADDR = 0x60
+MOTOR_CHANNEL_L = 3
+MOTOR_CHANNEL_R = 4
+TEST_SPEED = 150
+TEST_TIME = 2
+
+
+mh = Raspi_MotorHAT(addr=I2C_ADDR)
+lm = mh.getMotor(MOTOR_CHANNEL_L)
+rm = mh.getMotor(MOTOR_CHANNEL_R)
 
 def turn_off_motors():
     lm.run(Raspi_MotorHAT.RELEASE)
@@ -12,8 +19,8 @@ def turn_off_motors():
 
 atexit.register(turn_off_motors)
 
-lm.setSpeed(150)
-rm.setSpeed(150)
+lm.setSpeed(TEST_SPEED)
+rm.setSpeed(TEST_SPEED)
 lm.run(Raspi_MotorHAT.FORWARD)
 rm.run(Raspi_MotorHAT.FORWARD)
-time.sleep(2)
+time.sleep(TEST_TIME)
