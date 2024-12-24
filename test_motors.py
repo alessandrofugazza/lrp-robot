7f1e7410bc464551b7a0b412d89d4e7f3949dd4e
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2024-12-24 02:51:22 +0100

    add args controlled forward/backwards motion and input validation

--- test_motors.py
+++ test_motors.py
@@ -1,3 +1,4 @@
+import argparse
 from Raspi_MotorHAT import Raspi_MotorHAT
 import time
 import atexit
@@ -5,8 +6,27 @@ import atexit
 I2C_ADDR = 0x60
 MOTOR_CHANNEL_L = 3
 MOTOR_CHANNEL_R = 4
-TEST_SPEED = 150
-TEST_TIME = 2
+
+MIN_SPEED = -255
+MAX_SPEED = 255
+
+def int_in_range(int_value, min_value, max_value):
+    int_value = int(int_value)
+    if int_value < min_value or int_value > max_value:
+        raise argparse.ArgumentTypeError(f"Value {int_value} is out of range [{min_value}, {max_value}]")
+    return int_value
+
+parser = argparse.ArgumentParser(description='Select a test.')
+# parser.add_argument('--test_n', type=lambda x: int_in_range(x, 1, 2), required=True, help='1. Forward\n2. Spin')
+parser.add_argument('--mcls', type=lambda x: int_in_range(x, MIN_SPEED, MAX_SPEED), required=True, help='Speed for left motor.')
+parser.add_argument('--mcrs', type=lambda x: int_in_range(x, MIN_SPEED, MAX_SPEED), required=True, help='Speed for right motor.')
+parser.add_argument('--test_time', type=lambda x: int_in_range(x, 1, 60), required=True, help='Duration to run the motors in seconds (1-60).')
+
+args = parser.parse_args()
+
+
+# TEST_SPEED = 150
+# TEST_TIME = 2
 
 
 mh = Raspi_MotorHAT(addr=I2C_ADDR)
@@ -19,8 +39,22 @@ def turn_off_motors():
 
 atexit.register(turn_off_motors)
 
-lm.setSpeed(TEST_SPEED)
-rm.setSpeed(TEST_SPEED)
-lm.run(Raspi_MotorHAT.FORWARD)
-rm.run(Raspi_MotorHAT.FORWARD)
-time.sleep(TEST_TIME)
+left_motor_abs_speed = abs(args.mcls)
+right_motor_abs_speed = abs(args.mcrs)
+
+if args.mcls >= 0:
+    lm.run(Raspi_MotorHAT.FORWARD)
+else:
+    lm.run(Raspi_MotorHAT.BACKWARD)
+
+if args.mcrs >= 0:
+    rm.run(Raspi_MotorHAT.FORWARD)
+else:
+    rm.run(Raspi_MotorHAT.BACKWARD)
+
+lm.setSpeed(left_motor_abs_speed)
+rm.setSpeed(right_motor_abs_speed)
+
+# lm.run(Raspi_MotorHAT.FORWARD)
+# rm.run(Raspi_MotorHAT.FORWARD)
+time.sleep(args.test_time)
