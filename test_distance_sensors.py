d23acc357a2bf96db422db1ebce71104a87b27e2
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2025-01-17 20:44:02 +0100

    update distance sensor initialization and output formatting

--- test_distance_sensors.py
+++ test_distance_sensors.py
@@ -1,14 +1,15 @@
 import time
 from gpiozero import DistanceSensor
-# from gpiozero.pins.pigpio import PiGPIOFactory
+from gpiozero.pins.pigpio import PiGPIOFactory
 
-print("Prepare GPIO Pins v2")
-# sensor_l = DistanceSensor(echo=17, trigger=27, pin_factory=PiGPIOFactory())
-# sensor_r = DistanceSensor(echo=5, trigger=6, pin_factory=PiGPIOFactory())
-sensor_l = DistanceSensor(echo=17, trigger=27, queue_len=2)
-sensor_r = DistanceSensor(echo=5, trigger=6, queue_len=2)
+print("Prepare GPIO Pins v4")
+sensor_l = DistanceSensor(echo=17, trigger=27, pin_factory=PiGPIOFactory())
+sensor_r = DistanceSensor(echo=5, trigger=6, pin_factory=PiGPIOFactory())
+# sensor_l = DistanceSensor(echo=17, trigger=27, queue_len=2)
+# sensor_r = DistanceSensor(echo=5, trigger=6, queue_len=2)
 
 while True:
-    # print("Left: {l}, Right: {r}".format( l=sensor_l.distance * 100, r=sensor_r.distance * 100))
-    print("Right: {r}".format(r=sensor_r.distance * 100))
+    # print("Left: {r}".format( r=sensor_r.distance * 100))
+    # print("Left: {l}".format( r=sensor_l.distance * 100))
+    print(f"Left: {sensor_l.distance * 100:.2f}, Right: {sensor_r.distance * 100:.2f}")
     time.sleep(0.1)
