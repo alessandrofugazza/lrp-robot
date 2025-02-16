39cc8dd79b3760f6bd0c67098e277de83eb6194f
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2025-02-16 15:30:23 +0100

    test head sensor

--- code/sensors/test_distance_sensors.py
+++ code/sensors/test_distance_sensors.py
@@ -3,13 +3,14 @@ from gpiozero import DistanceSensor
 from gpiozero.pins.pigpio import PiGPIOFactory
 
 print("Prepare GPIO Pins v4")
-sensor_l = DistanceSensor(echo=17, trigger=27, pin_factory=PiGPIOFactory())
-sensor_r = DistanceSensor(echo=5, trigger=6, pin_factory=PiGPIOFactory())
+# sensor_l = DistanceSensor(echo=17, trigger=27, pin_factory=PiGPIOFactory())
+# sensor_r = DistanceSensor(echo=5, trigger=6, pin_factory=PiGPIOFactory())
+sensor_head = DistanceSensor(echo=24, trigger=23, pin_factory=PiGPIOFactory())
 # sensor_l = DistanceSensor(echo=17, trigger=27, queue_len=2)
 # sensor_r = DistanceSensor(echo=5, trigger=6, queue_len=2)
 
 while True:
     # print("Left: {r}".format( r=sensor_r.distance * 100))
     # print("Left: {l}".format( r=sensor_l.distance * 100))
-    print(f"Left: {sensor_l.distance * 100:.2f}, Right: {sensor_r.distance * 100:.2f}")
+    print(f"{sensor_head.distance * 100:.2f}")
     time.sleep(0.1)
